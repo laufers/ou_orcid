d8d670b191c9315b14cc99bdf5a4164daa547cdb
Mark Laufersweiler <laufers@gmail.com>
2015-12-17 14:01:41 -0600

    added generalizations and nested dictionary for output

--- ou_orcid.py
+++ ou_orcid.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import pprint
 import orcid
 import time
@@ -7,40 +9,45 @@ def search_persons(ringgoldid, place):
     api = orcid.PublicAPI()
     persons = {}
     counter = 1
-    person_counter = 0
+    row = 20
+    total_records = api.search_public("6187 University of Oklahoma")['orcid-search-results']['num-found']
+    total_records = 1000
     
-    for step in range(0,10,11):
+    for step in range(0, total_records, row):
         try:
-            uni = api.search_public('%s %s' % (ringgoldid, place), start=str(step), rows=20)
-            for result in uni.get('orcid-search-results').get('orcid-search-result'):
-                curent_data = {}
-                theid = result.get('orcid-profile').get('orcid-identifier').get('path')
-                fname = result.get('orcid-profile').get('orcid-bio').get('personal-details').get('given-names').get('value')
-                lname = result.get('orcid-profile').get('orcid-bio').get('personal-details').get('family-name').get('value')
-                print (fname + ' ' + lname)
+            university = api.search_public('%s %s' % (ringgoldid, place), start=str(step), rows=row)
+            for result in university.get('orcid-search-results').get('orcid-search-result'):
+                orcid_id = result.get('orcid-profile').get('orcid-identifier').get('path')
                 try:
-                    summary = api.read_record_public(theid, 'activities')
+                    fname = result.get('orcid-profile').get('orcid-bio').get('personal-details').get('given-names').get('value')
+                except:
+                    fname = ''
+                try:
+                    lname = result.get('orcid-profile').get('orcid-bio').get('personal-details').get('family-name').get('value')
+                except:
+                    lname = ''
+                
+                try:
+                    summary = api.read_record_public(orcid_id, 'activities')
                     try:
                         for employment in summary.get('employments').get('employment-summary'):
                             if employment.get('organization').get('disambiguated-organization').get('disambiguated-organization-identifier') == ringgoldid:
                                 if not employment.get('end-date'):
-                                    persons.setdefault(employment.get('department-name'), []).append(theid)
-                                    person_counter += 1
+                                    persons.setdefault(orcid_id, {}).setdefault((fname + ' ' + lname), []).append(employment.get('department-name'))
                     except AttributeError:
                         pass
                 except RequestException as e:
                     print (e.response.text)
                 print ('Processing record %d' % counter)
                 counter += 1
-            # print (person_counter)
             # pprint.pprint(persons)
         except RequestException as e:
             print (e.response.text)
-        time.sleep(30)
-    
-    print (person_counter)
-    pprint.pprint(persons)
+        #time.sleep(3)
+    return persons
 
-# search_persons('9142', 'Bochum')
-# search_persons('14311', 'Dortmund')
-search_persons ('6187', 'University of Oklahoma')
+    
+people = search_persons ('6187', 'University of Oklahoma')
+
+print ('Total number of employess at OU with ORCID: ', len(people))
+pprint.pprint(people)
