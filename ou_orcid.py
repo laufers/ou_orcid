7dc81eb5900ed55894a29b6f85990e89632b01ee
Mark Laufersweiler <laufers@gmail.com>
2016-01-06 17:02:14 -0600

    add progress meter and nolonger a function and fixed error trp

--- ou_orcid.py
+++ ou_orcid.py
@@ -4,7 +4,14 @@ import pprint
 import orcid
 import time
 from requests import RequestException
+from progressbar import AnimatedMarker, Bar, BouncingBar, Counter, ETA, \
+    FileTransferSpeed, FormatLabel, Percentage, \
+    ProgressBar, ReverseBar, RotatingMarker, \
+    SimpleProgress, Timer, AdaptiveETA, AbsoluteETA, AdaptiveTransferSpeed
 
+
+
+# recurse function allows for recursivly printing out m
 def recurse(d, keys=()):
     if type(d) == dict:
          for k in d:
@@ -14,73 +21,82 @@ def recurse(d, keys=()):
         yield (keys, d)
 
 
-def search_persons(ringgoldid, place):
-    api = orcid.PublicAPI()
-    persons = {}
-    counter = 1
-    row = 20
-    total_records = api.search_public('%s %s' % (ringgoldid, place))['orcid-search-results']['num-found']
-    print ('The total number of entries matching', ringgoldid, 'and', place, 'is', total_records)
-    total_records = 200
+#  Set or calculate program varaiables here
 
-    # define log entries
-    tmpl = '%s\t%s\t%s\n'
+api = orcid.PublicAPI()
+people = {}
+counter = 0
+stepcount = 20
+ringgoldid = '6187'   
+place = 'University of Oklahoma'
 
-    for step in range(0, total_records, row):
-        try:
-            university = api.search_public('%s %s' % (ringgoldid, place), start=str(step), rows=row)
-            for result in university.get('orcid-search-results').get('orcid-search-result'):
-                orcid_id = result.get('orcid-profile').get('orcid-identifier').get('path')
-              #  request_log.write(tmpl % (datetime.isoformat(datetime.now()), counter, orcid_id))
-                
-                try:
-                    fname = result.get('orcid-profile').get('orcid-bio').get('personal-details').get('given-names').get('value')
-                except:
-                    fname = ''
-                try:
-                    lname = result.get('orcid-profile').get('orcid-bio').get('personal-details').get('family-name').get('value')
-                except:
-                    lname = ''
-                
-                try:
-                    summary = api.read_record_public(orcid_id, 'activities')
-                    try:
-                        for employment in summary.get('employments').get('employment-summary'):
-                            if employment.get('organization').get('disambiguated-organization').get('disambiguated-organization-identifier') == ringgoldid:
-                                if not employment.get('end-date'):
-                                    persons.setdefault(orcid_id, {}).setdefault((fname + ' ' + lname), []).append(employment.get('department-name'))
-                                request_log.write(tmpl % (datetime.isoformat(datetime.now()), counter, orcid_id))
+total_records = api.search_public('%s %s' % (ringgoldid, place))['orcid-search-results']['num-found']
+total_records = 100
+print('The total number of entries matching %s and %s is %s.\n' % (ringgoldid, place, total_records))
 
-                    except AttributeError:
-                        pass
-                except RequestException as e:
-                    print (e.response.text)
-                #print ('Processing record %d' % counter)
-                counter += 1
-            # pprint.pprint(persons)
-        except RequestException as e:
-            print (e.response.text)
-        #time.sleep(3)
-    return persons
 
-# Start program run here
 # define log file
 import os
 from datetime import datetime
 if not os.path.exists('output'):
-	os.makedirs('output')
+    os.makedirs('output')
     
 # log 
 request_log = open('output/orcid.log', 'w')
 request_log.write('Timestamp\t\t\tstep\tORCID\n')
+# define log entries template
+tmpl = '%s\t%s\t%s\n'
+    
+#  Progress bar setup
+pbar = ProgressBar(widgets=['Working: ', Percentage(), Bar()], max_value=total_records + stepcount).start()
+# pbar = ProgressBar(widgets=['Working: ', Percentage(), Bar()]).start()
+# pbar = ProgressBar(widgets=['Working: ', Percentage(), Bar()]).start()
 
+for step in range(0, total_records, stepcount):
+    try:
+        university = api.search_public('%s %s' % (ringgoldid, place), start=str(step), rows=stepcount)
+        for result in university.get('orcid-search-results').get('orcid-search-result'):
+            orcid_id = result.get('orcid-profile').get('orcid-identifier').get('path')
+            
+            try:
+                fname = result.get('orcid-profile').get('orcid-bio').get('personal-details').get('given-names').get('value')
+            except:
+                fname = ''
+            try:
+                lname = result.get('orcid-profile').get('orcid-bio').get('personal-details').get('family-name').get('value')
+            except:
+                lname = ''
+            
+            try:
+                summary = api.read_record_public(orcid_id, 'activities')
+                try:
+                    for employment in summary.get('employments').get('employment-summary'):
+                        if employment.get('organization').get('disambiguated-organization').get('disambiguated-organization-identifier') == ringgoldid:
+                            if not employment.get('end-date'):
+                                people.setdefault(orcid_id, {}).setdefault((fname + ' ' + lname), []).append(employment.get('department-name'))
+                            # request_log.write(tmpl % (datetime.isoformat(datetime.now()), counter, orcid_id))
+                    request_log.write(tmpl % (datetime.isoformat(datetime.now()), counter, orcid_id))
 
-# Call function for search 
-   
-people = search_persons ('6187', 'University of Oklahoma')
+                except AttributeError:
+                    pass
+            except Exception as e:
+                # print (e)
+                request_log.write(tmpl % (datetime.isoformat(datetime.now()), orcid_id, e))
+            #print ('Processing record %d' % counter)
+            counter += 1
+            # print(counter)
+            pbar += 1
+        # pbar += stepcount
+        # pprint.pprint(people)
+    except Exception as e:
+        request_log.write(tmpl % (datetime.isoformat(datetime.now()), orcid_id, e))
+    # pbar.update(step)
+    #time.sleep(3)
 
-print ('Total number of employess at OU with ORCID: ', len(people))
-pprint.pprint(people)
+pbar.finish()
+
+request_log.write('The total number of entries matching %s and %s is %s.\n' % (ringgoldid, place, total_records))
+request_log.write('Total number of employess at OU with ORCID: %s\n' % len(people))
 
 # Output to csv file
 import csv
@@ -90,3 +106,4 @@ with open ('output/ou_orcid.csv', 'w', newline='') as fileout:
     for ids, val in recurse(people):
             peoplewriter.writerow([ j for j in ids] +  [k for k in val])
 fileout.close()
+request_log.close()
