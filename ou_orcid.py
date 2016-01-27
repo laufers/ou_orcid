71e85eaf20a1a5713c9714d3a0246f95ca46387b
Mark Laufersweiler <laufers@gmail.com>
2016-01-27 12:44:43 -0600

    changing search limits and pprogress meter tweaks

--- ou_orcid.py
+++ ou_orcid.py
@@ -26,13 +26,18 @@ def recurse(d, keys=()):
 api = orcid.PublicAPI()
 people = {}
 counter = 0
-stepcount = 20
+stepcount = 25
 ringgoldid = '6187'   
 place = 'University of Oklahoma'
 
-total_records = api.search_public('%s %s' % (ringgoldid, place))['orcid-search-results']['num-found']
-total_records = 100
-print('The total number of entries matching %s and %s is %s.\n' % (ringgoldid, place, total_records))
+# search = ringgoldid + ' ' + place
+# search = place 
+search = ringgoldid
+
+total_records = api.search_public('%s' % (search))['orcid-search-results']['num-found']
+# total_records = api.search_public('%s %s' % (ringgoldid, place))['orcid-search-results']['num-found']
+# total_records = 8000
+print('The total number of entries matching %s is %s.\n' % (search, total_records))
 
 
 # define log file
@@ -40,21 +45,32 @@ import os
 from datetime import datetime
 if not os.path.exists('output'):
     os.makedirs('output')
+
+#  File output names 
+logfile = datetime.now().strftime('%Y%m%d%H%M_orcid.log')
+csvfile = datetime.now().strftime('%Y%m%d%H%M_orcid.csv')
+
+
     
 # log 
-request_log = open('output/orcid.log', 'w')
+request_log = open(('output/' + logfile), 'w')
 request_log.write('Timestamp\t\t\tstep\tORCID\n')
 # define log entries template
 tmpl = '%s\t%s\t%s\n'
+
+# raise SystemExit 
     
 #  Progress bar setup
-pbar = ProgressBar(widgets=['Working: ', Percentage(), Bar()], max_value=total_records + stepcount).start()
+widgets = ['Working: ', AnimatedMarker(markers='←↖↑↗→↘↓↙')]
+pbar = ProgressBar(widgets=widgets, max_value=total_records + stepcount).start()
 # pbar = ProgressBar(widgets=['Working: ', Percentage(), Bar()]).start()
 # pbar = ProgressBar(widgets=['Working: ', Percentage(), Bar()]).start()
 
 for step in range(0, total_records, stepcount):
     try:
-        university = api.search_public('%s %s' % (ringgoldid, place), start=str(step), rows=stepcount)
+        # print (step)
+        university = api.search_public('%s' % (search), start=str(step), rows=stepcount)
+        # university = api.search_public('%s' % (search), start=str(step), rows=stepcount)
         for result in university.get('orcid-search-results').get('orcid-search-result'):
             orcid_id = result.get('orcid-profile').get('orcid-identifier').get('path')
             
@@ -75,7 +91,7 @@ for step in range(0, total_records, stepcount):
                             if not employment.get('end-date'):
                                 people.setdefault(orcid_id, {}).setdefault((fname + ' ' + lname), []).append(employment.get('department-name'))
                             # request_log.write(tmpl % (datetime.isoformat(datetime.now()), counter, orcid_id))
-                    request_log.write(tmpl % (datetime.isoformat(datetime.now()), counter, orcid_id))
+                    # request_log.write(tmpl % (datetime.isoformat(datetime.now()), counter, orcid_id))
 
                 except AttributeError:
                     pass
@@ -86,11 +102,9 @@ for step in range(0, total_records, stepcount):
             counter += 1
             # print(counter)
             pbar += 1
-        # pbar += stepcount
         # pprint.pprint(people)
     except Exception as e:
         request_log.write(tmpl % (datetime.isoformat(datetime.now()), orcid_id, e))
-    # pbar.update(step)
     #time.sleep(3)
 
 pbar.finish()
@@ -101,9 +115,10 @@ request_log.write('Total number of employess at OU with ORCID: %s\n' % len(peopl
 # Output to csv file
 import csv
 
-with open ('output/ou_orcid.csv', 'w', newline='') as fileout:
+with open (('output/' + csvfile), 'w', newline='') as fileout:
     peoplewriter = csv.writer(fileout, delimiter=',')
     for ids, val in recurse(people):
             peoplewriter.writerow([ j for j in ids] +  [k for k in val])
 fileout.close()
 request_log.close()
+print ('Done\n\n')
